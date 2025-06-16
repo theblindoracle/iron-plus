d7eb6e17a3a9fc45af36d46b8d65b5f50d9014af
Travis Napier <napesdotnet@gmail.com>
2025-06-16 12:50:31 -0400

    update app and app shell

--- IronPlus/AppShell.xaml.cs
+++ IronPlus/AppShell.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Controls;
+﻿using IronPlus.Views;
+using Microsoft.Maui.Controls;
 
 namespace IronPlus;
 
@@ -7,5 +8,16 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute("showSpecificWeight", typeof(ShowSpecificWeightPage));
+		Routing.RegisterRoute("howToUseRpeChart", typeof(HowToUseRpeChartPage));
+		Routing.RegisterRoute("howToUseUnitConversion", typeof(HowToUseUnitConversionPage)); 
+		Routing.RegisterRoute("howToUseWarmUpCalculation", typeof(HowToUseWarmUpCalculationPage));
+		Routing.RegisterRoute("about", typeof(AboutPage));
+		Routing.RegisterRoute("themeSettings", typeof(ThemeSettingsPage));
+		Routing.RegisterRoute("barbellSettings", typeof(BarbellSettingsPage));
+		Routing.RegisterRoute("addBarbellDetails", typeof(AddBarbellDetailsPage));
+		Routing.RegisterRoute("rpeChartSettings", typeof(RPEChartSettingsPage));
+		Routing.RegisterRoute("generalSettings", typeof(GeneralSettingsPage));
 	}
 }
