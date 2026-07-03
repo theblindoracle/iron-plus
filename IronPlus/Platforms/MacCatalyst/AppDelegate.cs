912f325375ab72714c54d328369d6056b71ffef5
Travis Napier <travis@Traviss-MacBook-Pro-3.local>
2026-07-03 14:28:10 -0400

    update icons and splash screens

--- IronPlus/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace IronPlus;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
